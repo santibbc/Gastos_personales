0100ff6f25c74de86c0f489aef1e77b09cac87c4
Maximiliano B. <maxibg1708@gmail.com>
2025-09-17 22:37:46 -0500

    commit 10:37

--- ut_presentac_impl/Repositorios/ProveedoresServicioPruebaslmp.cs
+++ ut_presentac_impl/Repositorios/ProveedoresServicioPruebaslmp.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using lib_dominio.Entidades;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ut_presentac_imp.Nucleo;
 
-namespace ut_presentac_impl.Repositorios
+namespace ut_presentac_imp.Repositorios
 {
-    internal class ProveedoresServicioPruebaslmp
+    [TestClass]
+    public class ProveedoresServiciosPruebaslmp
     {
+        private readonly IConexion? iConexion;
+        private ProveedoresServicios? entidad;
+        private ProveedoresServiciosAplicacion? app;
+
+        public ProveedoresServiciosPruebaslmp()
+        {
+            iConexion = new Conexion();
+            iConexion.StringConexion = Configuracion.ObtenerValor("StringConexion");
+            app = new ProveedoresServiciosAplicacion(iConexion);
+        }
+
+        [TestMethod]
+        public void Ejecutar()
+        {
+            Assert.AreEqual(true, Guardar());
+            Assert.AreEqual(true, Modificar());
+            Assert.AreEqual(true, Listar());
+            Assert.AreEqual(true, Borrar());
+        }
+
+        public bool Guardar()
+        {
+            entidad = new ProveedoresServicios
+            {
+                IdProveedor = 1,
+                Banco = "Banco Prueba",
+                NumeroCuenta = $"TEST-{Guid.NewGuid().ToString().Substring(0, 8)}",
+                TipoCuenta = "Ahorros",
+                SaldoInicial = 0,
+                Moneda = "COP"
+            };
+
+            app!.Guardar(entidad);
+            return entidad.IdProveedor > 0;
+        }
+
+        public bool Modificar()
+        {
+            entidad!.Banco = "Banco Modificado";
+            app!.Modificar(entidad);
+            return true;
+        }
+
+        public bool Listar()
+        {
+            var lista = app!.Listar();
+            return lista.Count > 0;
+        }
+
+        public bool Borrar()
+        {
+
+            app!.Borrar(entidad);
+            return true;
+        }
     }
 }
+
+
+
